d809769491643703db62794cdf323223ea14be10
Prashanth <prashanthr261993@gmail.com>
2016-09-22 22:47:10 -0400

    Added compose feature to message.
    Tweaked few javascript code related to sidebar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( style.scss elegant-icons-style.css tags/index.scss questions/index.scss)
+Rails.application.config.assets.precompile += %w( style.scss elegant-icons-style.css tags/index.scss questions/index.scss messages/inbox.scss messages/new.scss)
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   resources :questions do
 
   end
+
+  resources :messages do
+    get :inbox, on: :collection
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
